7b5b32f499315857236542b9702ddc16bcf1aa9f
Navwojah-Ariko_Eloho <arikoeloho1996@gmail.com>
2023-12-17 14:15:21 +0100

    Add services section

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -3,27 +3,52 @@ import './Services.css'
 import glasses from '../../images/glasses.png'
 import heartmoji from "../../images/heartmoji.png";
 import humble from "../../images/humble.png";
+import Card from '../Card/Card';
+import resume from './Eloho N.A..docx'
 
 const Services = () => {
   return (
-    <div className='services'>
-        {/* Left Side */}
-        <div className="awesome">
-            <span>My Awesome</span>
-            <span>services</span>
-            <span>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. <br/> Incidunt totam sequi pariatur quidem aperiam commodi quo quam
-            </span>
+    <div className="services">
+      {/* Left Side */}
+      <div className="awesome">
+        <span>My Awesome</span>
+        <span>services</span>
+        <span>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. <br />{" "}
+          Incidunt totam sequi pariatur quidem aperiam commodi quo quam
+        </span>
+        <a href={resume} download>
             <button className="button s-button">Download CV</button>
-            <div className="blur s-blur"></div>
-        </div>
+          </a>
+        <div className="blur s-blur"></div>
+      </div>
 
-        {/* Right Side */}
-        <div className="cards">
-            Right Side
+      {/* Right Side */}
+      <div className="cards">
+        <div style={{ left: "14rem" }}>
+          <Card
+            emoji={heartmoji}
+            heading={"Design "}
+            detail={"HTML5, CSS3, Vanilla JavaScript"}
+          />
         </div>
+        <div style={{ top: "12rem", left: "-4rem" }}>
+          <Card
+            emoji={glasses}
+            heading={"Version Control "}
+            detail={"Git and GitHub"}
+          />
+        </div>
+        <div style={{ top: "19rem", left: "12rem" }}>
+          <Card
+            emoji={humble}
+            heading={"Soft Skills "}
+            detail={"Communication, Team work, Problem solving and Critical thinking"}
+          />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Services
